12d574159274d8afc0345be5bc97868fc7c753ca
Michael Ottinger <michael@ottinger.net>
2019-11-28 02:57:38 -0500

    Change to random forest model with lat/lon added and land value removed

--- model.py
+++ model.py
@@ -26,12 +26,12 @@ def read_sales_csv(filename):
                      'price': int(row['price'])})
     return sales
 
-conn = sqlite3.connect('file:NW_Central_OKC_w_bldg_details.db?mode=ro', uri=True)
+conn = sqlite3.connect('file:data/NW_Central_OKC_w_bldg_details.db?mode=ro', uri=True)
 c = conn.cursor()
 
-sales = read_sales_csv('sales_list_2019.csv')
-#sales = sales + read_sales_csv('sales_list_2018.csv')
-#sales = sales + read_sales_csv('sales_list_2017.csv')
+sales = read_sales_csv('data/sales_list_2019.csv')
+#sales = sales + read_sales_csv('data/sales_list_2018.csv')
+#sales = sales + read_sales_csv('data/sales_list_2017.csv')
 
 
 # Now let's load the data into DataFrames. We have two tables we're using, one for properties/parcels and one for buildings. So what we'll do is pull out some data about the buildings, sum it up (as some properties have more than 1 building), and add it to the property DF.
@@ -51,7 +51,9 @@ rp_df['bath_sum'] = combined_df.replace(-1,0).groupby(['account_number'])['full_
 rp_df['room_sum'] = combined_df.replace(-1,0).groupby(['account_number'])['total_rooms'].sum()
 rp_df['main_bldg_sqft'] = combined_df.groupby(['account_number'])['sq_ft'].max()
 rp_df['year_built'] = combined_df[combined_df.year_built != -1].groupby(['account_number'])['year_built'].mean()
-rp_df['bldg_age'] = pd.datetime.now().year - rp_df['year_built']
+
+rp_df.head()
+
 
 # Now let's add more features for the main building. To do this we'll create a new DataFrame containing the features of the largest building on each property. While most single-family properties only have one building, we do need to consider that some will have multiple.
 
@@ -60,8 +62,6 @@ rp_df['bldg_age'] = pd.datetime.now().year - rp_df['year_built']
 
 # we want 26584 not 26631
 idx = combined_df.groupby(['account_number'])['sq_ft'].transform(max) == combined_df['sq_ft']
-pd.set_option('display.max_columns', 60)
-pd.set_option('display.max_rows', 100)
 combined_df2 = combined_df[idx]
 
 
@@ -70,7 +70,7 @@ combined_df2 = combined_df[idx]
 idx = combined_df2.groupby(['account_number'])['bldg_id'].transform(max) == combined_df2['bldg_id']
 combined_df2 = combined_df2[idx]
 combined_df2 = combined_df2.set_index(['account_number'])
-combined_df2
+combined_df2.head()
 
 
 # Now we have the features for the largest building on each parcel. Let's add some of these to combined_df (like we did above).
@@ -78,8 +78,6 @@ combined_df2
 # In[5]:
 
 
-print(len(rp_df))
-print(len(combined_df2))
 #print(combined_df2.groupby(['account_number'])['bldg_description'])
 rp_df['main_bldg_description'] = combined_df2['bldg_description']
 rp_df['main_bldg_hvac'] = combined_df2['hvac_type']
@@ -89,7 +87,7 @@ rp_df['main_bldg_exterior'] = combined_df2['exterior']
 rp_df['main_bldg_roof'] = combined_df2['roof_type']
 rp_df['main_bldg_year'] = combined_df2['year_built']
 rp_df['main_bldg_effective_year'] = combined_df2[['year_built', 'remodel_year']].max(axis=1) # remodel year, else built year
-rp_df
+rp_df.head()
 
 
 # In[6]:
@@ -101,8 +99,8 @@ sublist = (rp_df[['subdivision']]
             .assign(count = 0)
             .groupby("subdivision")
             .count())
-sublist = sublist.where(sublist['count'] >= 100).dropna()
 sublist_names = sublist.index.values
+
 rp_df['subdivision'] = rp_df.apply(lambda x: "Other" if x['subdivision'] not in sublist_names else x['subdivision'], axis=1)
 
 
@@ -110,20 +108,20 @@ rp_df['subdivision'] = rp_df.apply(lambda x: "Other" if x['subdivision'] not in 
 
 
 sales_df = pd.DataFrame(sales)
-
+sales_df.head()
 
 
 # In[8]:
 
 
 merged_df = pd.merge(rp_df, sales_df, on='account_number', how='inner')
+merged_df.head()
 
 
 # In[9]:
 
 
-included_fields = ['land_size', 'land_value', 'subdivision', 'bldg_age', 'year_built', 'sqft_sum', 'bed_sum', 'bath_sum', 'price', 'days_ago', 'main_bldg_hvac', 'main_bldg_description', 'main_bldg_exterior', 'main_bldg_quality', 'main_bldg_roof', 'main_bldg_year', 'main_bldg_effective_year', 'room_sum']
-new_df = merged_df[included_fields]
+included_fields = ['land_size', 'land_value', 'subdivision', 'year_built', 'sqft_sum', 'bed_sum', 'bath_sum', 'price', 'days_ago', 'main_bldg_hvac', 'main_bldg_description', 'main_bldg_exterior', 'main_bldg_quality', 'main_bldg_roof', 'main_bldg_year', 'main_bldg_effective_year', 'room_sum']
 
 
 # In[10]:
@@ -134,7 +132,7 @@ from sklearn import preprocessing
 
 
 # ### One Hot Encoding
-#
+# 
 # We use one hot encoding for the subdivision feature. As a result, we create a feature for each individual neighborhood and other categorical features.
 
 # In[11]:
@@ -177,6 +175,7 @@ encoded_df_names = [
 ]
 
 encoded_dfs = []
+encoders = {}
 for (col_name, prefix) in encoded_df_names:
     cur_df = return_encoded(new_df, col_name, prefix)
     encoded_dfs.append(cur_df)
@@ -189,47 +188,24 @@ unused_col_names = ['main_bldg_quality']
 
 new_df = new_df.drop(columns=([x[0] for x in encoded_df_names]+[x for x in unused_col_names]))
 new_df = pd.concat(([new_df] + [x for x in encoded_dfs]), axis=1)
-
-
-# In[ ]:
-
-
-
+new_df.head()
 
 
 # ### Correlation
-#
+# 
 # Let's look at the correlation of the various featues. We see that some features like square footage, # of beds/baths, and land size have a positive correlation.
-#
+# 
 # Some of the more desirable subdivisions have a positive correlation too, while many other neighborhoods have a negative correlation with the sale price (relative to other sales in our data set). Do note that this is correlation for the price of the property - not price relative to building or land size.
-#
+# 
 # Year built/building age is close enough to zero to have no correlation. And as we might expect, days_ago has a negative correlation, indicating that prices have generally been increasing over time.
 
 # In[16]:
 
 
-pd.set_option('max_rows',100)
 dfcorr = new_df.corr()
-new_df
-
-
-# In[17]:
-
 
 pd.set_option('max_rows',200)
 
-
-# In[18]:
-
-
-new_df = new_df.drop('bldg_age',axis=1)
-
-
-# ### Creating a linear regression model
-#
-# Let's create our linear regression model.
-
-# In[19]:
 
 
 X = new_df.drop('price',axis=1)
@@ -238,7 +214,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 linreg = LinearRegression().fit(X_train, y_train)
 
 
-# In[20]:
+# In[19]:
 
 
 from sklearn.metrics import mean_squared_error
@@ -252,57 +228,78 @@ def model_stats(model, Xte, yte, Xtr, ytr, yp):
 
     print("y_test min: " + str(y_test.min()))
     print("y_test max: " + str(y_test.max()))
+    print("y_test mean: " + str(y_test.mean()))
     print("y_predict min: " + str(yp.min()))
     print("y_predict max: " + str(yp.max()))
+    print("y_predict mean: " + str(yp.mean()))
 
 
-# And if we look at the predicted values, some turn out to be negative. Uh oh.
+def plot_test_predict(Xt,yp,yt):
+    xy_test_df = pd.concat([Xt.reset_index(), pd.Series(yp), yt.reset_index()],axis=1)
 
-# In[21]:
+    plt.ylim(xy_test_df[0].min() - 20000, xy_test_df[0].max() + 20000)
+    plt.xlim(xy_test_df['price'].min() - 20000, xy_test_df['price'].max() + 20000)
 
-
-'''
-import numpy as np
-ch_oh = ohenc.transform([["EDGEMERE TERRACE"]]).toarray()
-ch_args = [10000, 40000, 1990, 3000, 4, 3, 10] + ch_oh.tolist()[0]
-ch_args = [6400, 34790, 1500, 1990, 4, 3, 10] + ch_oh.tolist()[0]
-#ch_args = ch_oh.tolist()
-linreg.predict([ch_args])
-'''
-
-y_predict = linreg.predict(X_test)
-model_stats(linreg,X_test,y_test,X_train,y_train,y_predict)
+    plt.plot(xy_test_df['price'], xy_test_df[0], 'ro')
 
 
-# In[22]:
+
+centroids = pd.read_csv('data/property_centroids.csv')
+print(centroids)
 
 
-# ### Bayesian Ridge with Polynomial Features
-
-# In[27]:
+# In[43]:
 
 
-from sklearn.linear_model import BayesianRidge
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import PolynomialFeatures
-# bayesian ridge worked well-ish
-# Crashes for PolynomialFeatures degree 3 though. Not sure why...
-bayesian_ridge = make_pipeline(PolynomialFeatures(2), BayesianRidge())
-bayesian_ridge.fit(X_train, y_train)
+merged_df = pd.merge(merged_df, centroids, left_on='account_number', right_on='accountno', how='inner')
+included_fields = included_fields + ['lat','lon']
+new_df = (merged_df[included_fields][merged_df.price != 0]
+          .dropna()
+          .reset_index()
+          .drop(columns=['index']))
 
 
-# In[28]:
+# In[44]:
 
 
-bayesian_ridge.score(X_test, y_test)
+# Now we have the features we had before, along with the latitude and longitude! Now let's convert the categorical features to one hot encoding and see how it performs.
+
+# In[45]:
 
 
-# In[29]:
+encoded_dfs = []
+encoders = {}
+for (col_name, prefix) in encoded_df_names:
+    cur_df = return_encoded(new_df, col_name, prefix)
+    encoded_dfs.append(cur_df)
+    
+new_df = new_df.drop(columns=([x[0] for x in encoded_df_names]+[x for x in unused_col_names]))
+new_df = pd.concat(([new_df] + [x for x in encoded_dfs]), axis=1)
+
+X = new_df.drop('price',axis=1)
+y = new_df['price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 
-y_predict = bayesian_ridge.predict(X_test)
+# ### Random Forest w/latlon
 
-model_stats(bayesian_ridge,X_test,y_test,X_train,y_train,y_predict)
+# In[48]:
 
-pickle.dump(bayesian_ridge, open('model.pkl', 'wb'))
+
+from sklearn.ensemble import RandomForestRegressor
+
+
+# RandomForest, no land value, with latlon
+X = X.drop('land_value',axis=1)
+
+X_train_no_lv = X_train.drop('land_value',axis=1)
+X_test_no_lv = X_test.drop('land_value',axis=1)
+
+forestreg_no_lv = RandomForestRegressor(n_estimators=100, max_depth=17).fit(X_train_no_lv, y_train)
+y_predict_no_lv = forestreg_no_lv.predict(X_test_no_lv)
+model_stats(forestreg_no_lv,X_test_no_lv,y_test,X_train_no_lv,y_train,y_predict_no_lv)
+
+
+
+pickle.dump(forestreg_no_lv, open('model.pkl', 'wb'))
 pickle.dump(X.columns, open('column_names.pkl', 'wb'))
